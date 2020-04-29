d15d3494dc71825575b373438f1d1ea42fe891d7
Derek Haynes <derek.haynes@gmail.com>
2020-04-29 06:41:03 -0600

    Fixing task loading

--- {{ cookiecutter.repo_name }}/tasks.py
+++ {{ cookiecutter.repo_name }}/tasks.py
@@ -1,10 +1,12 @@
 from invoke import Collection, task
 # Default tasks are in core.tasks
-from core.tasks import app, notebooks
-# Adds in default tasks
+import core.tasks.app as app
+import core.tasks.notebooks as notebooks
+# Add default task collections
 ns = Collection()
 ns.add_collection(app)
+ns = Collection(app)
 ns.add_collection(notebooks)
 
-### Add project-specific tasks below.
+### Add project-specific tasks below. ###
 # See http://docs.pyinvoke.org/en/stable/getting-started.html
